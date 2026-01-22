02e2c895b702f24f12238e9772557a1348b2dce9
Daniel-Sinkin <danielsinkin97@gmail.com>
2026-01-22 02:59:37 +0100

    Initial set up

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # GetCrackedIO
 https://getcracked.io/
+
+
+```
+cmake -S . -B build -DCMAKE_BUILD_TYPE=Debug
+```
---
+++ src/main.cpp
@@ -0,0 +1,26 @@
+// src/main.cpp
+#include "pch.hpp"
+
+//
+
+namespace ds_gc
+{
+using usize = std::size_t;
+using isize = std::ptrdiff_t;
+
+using i16 = std::int16_t;
+using i32 = std::int32_t;
+using i64 = std::int64_t;
+using i128 = __int128;
+
+using u16 = std::uint16_t;
+using u32 = std::uint32_t;
+using u64 = std::uint64_t;
+using u128 = __uint128_t;
+
+}  // namespace ds_gc
+
+int main()
+{
+    std::println("Hello, World");
+}
